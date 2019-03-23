b856e42a7dd8426e5ab0d1a4bc81d5ffd296488c
raspberry <raspberry>
2019-03-23 13:51:49 +0200

    image saver skelethon

---
+++ camera/camera_processings.py
@@ -0,0 +1,21 @@
+import time
+import datetime
+import picamera
+
+
+class ImageSaver():
+    
+    def __init__(self):
+        self.queue = []
+        self.camera = picame.PiCamera()
+        self.rate = 30
+        self.f = ""
+        self.fname = "{}.jpeg"
+    
+    def start(self):
+        pass
+    
+    def capture():
+        pass
+    
+        
