352b6f5b012d53148a092cb3ed0b39e11a10a64b
raspberry <raspberry>
2019-03-24 09:24:05 +0200

    save last angles in pickle

--- camera/raspberry_to_arduino.py
+++ camera/raspberry_to_arduino.py
@@ -1,7 +1,9 @@
 import serial
 import time
 import sys
+import pickle
 
+pickle_dir = "./pickle/motor_angles.pickle"
 device = "/dev/ttyACM0"
 rotate_motors = "RM {} {}\r"
 move_motors = "DM {}\r"
@@ -10,59 +12,8 @@ arguments_length = 3
 move_command = "move"
 rotate_command = "rotate"
 
-initialization_motor_osses = {
-    1 :90,
-    2 :90,
-    2 :90,
-    3 :90,
-    4 :90,
-    5 :90,
-    6 :90,
-    7 :90
-    }
-
-last_motor_osses = {
-    1 :90,
-    2 :90,
-    2 :90,
-    3 :90,
-    4 :90,
-    5 :90,
-    6 :90,
-    7 :90
-    }
-
-
-
-
-ser = serial.Serial(device,9600)
-#ser.write(str.encode('a'))
-#for i in range(0,100):
-#   ser.write(str.encode('a'))
-#   time.sleep(1)
-# 
-
-def parse_arguments():
-    print(sys.argv)
-#    if len(sys.argv) < arguments_length:
-#        return
+#ser = serial.Serial(device,9600)
     
-    command = "command"
-
-    if sys.argv[1] == move_command:
-        left_motor = sys.argv[2]
-#        right_motor = sys.argv[3]
-        command = move_motors.format(left_motor)
-    elif sys.argv[1] == rotate_command:
-        motor_number = sys.argv[2]
-        motor_angle = sys.argv[3]
-        command = rotate_motors.format(motor_number,motor_angle)
-    else:
-        command = stop_motors
-    print(command)
-    return command
-    
-
 def send_command():
     command = parse_arguments()
     print("i will send this command {}".format(command))
@@ -81,37 +32,46 @@ def move_motors_command(left_motor,right_motor):
     stop_motors_command()
     
 
-def send_rotate_commands(motor_number,motor_angle):
-    step = 5
-    begin = 0
-    end = 0
-    last_rotated_os = last_motor_osses[motor_number]
-    if last_rotated_os > motor_angle:
-            step = -step
-            begin = last_rotated_os
+def send_rotate_command(motor_number,motor_angle):
+    try:
+        step = 5
+        begin = 0
+        end = 0
+        last_motor_osses_dict = load_motors_last_angles()
+        motor_number = int(motor_number)
+        motor_angle= int(motor_angle)
+        print('last',  last_motor_osses_dict)
+        last_rotated_angle = last_motor_osses_dict[motor_number]
+        if last_rotated_angle > motor_angle:
+                begin = last_rotated_angle
+                end = motor_angle
+        else:
+            begin = last_rotated_angle
             end = motor_angle
-    else:
-        begin = motor_angle
-        end = last_rotated_os
+        if begin > end:
+            step = -step
+            
+        for i in range(begin,end,step):
+            command = rotate_motors.format(motor_number,i)
+            send_signal_command(command)
+            
+        last_motor_osses_dict[motor_number] = motor_angle
+        save_motors_last_angles(last_motor_osses_dict)
+        print('new',  last_motor_osses_dict)
         
-    for i in range(begin,end,step):
-        command = rotate_motors.format(motor_number,i)
-#    
-    pass
-
+    except Exception as e:
+        print(e)
+        command = rotate_motors.format(motor_number,motor_angle)
+        send_signal_command(command)
 
 
 
 def send_signal_command(command):
-    ser.write(str.encode(command))
+    #ser.write(str.encode(command))
+    print(command)
 
-
-def parse_arguments_new_function():
+def parse():
     print(sys.argv)
-#    if len(sys.argv) < arguments_length:
-#        return
-    
-    command = "command"
 
     if sys.argv[1] == move_command:
         left_motor = sys.argv[2]
@@ -120,15 +80,24 @@ def parse_arguments_new_function():
     elif sys.argv[1] == rotate_command:
         motor_number = sys.argv[2]
         motor_angle = sys.argv[3]
-        command = rotate_motors.format(motor_number,motor_angle)
+        send_rotate_command(motor_number,motor_angle)
     else:
         command = stop_motors
-    print(command)
-    return command
+   
 
 
+def save_motors_last_angles(a):
+    with open(pickle_dir, 'wb') as handle:
+        pickle.dump(a, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+def load_motors_last_angles():
+    with open(pickle_dir, 'rb') as handle:
+        b = pickle.load(handle)
+        return b
+    return None
 
 
 if __name__ == "__main__":
-    send_command()
+    parse()
 #    pass
+   
